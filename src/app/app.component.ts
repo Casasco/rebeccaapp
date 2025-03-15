b8e811989ec74ffb7b5e1ac9a8530a37fa15b69d
Casasco <gabrielcasasco2@gmail.com>
2025-03-15 19:43:48 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rebeccaapp';
+  title = 'apprebecca';
 }
---
+++ src/app/componentes/header/header.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  daysRemaining: number = 0;
+  mensagemDia: string = 'dias';
+  mensagemFalta: string ='Faltam';
+  mensagemHeader: string = '';
+
+  ngOnInit(): void {
+    this.calculateDaysRemaining();
+  }
+
+  calculateDaysRemaining(): void {
+    var targetDate = new Date('2025-03-23'); // Data específica
+    const currentDate = new Date();
+    const currentDateString = currentDate.toISOString().split('T')[0];
+    var targetDateString = targetDate.toISOString().split('T')[0];
+    
+    if(currentDateString === targetDateString){
+      this.mensagemHeader = 'Hoje a Rebecca se mudou :(';
+      return;
+    }
+    if(currentDate > targetDate){
+      targetDate = new Date('2025-07-25');
+      targetDateString = targetDate.toISOString().split('T')[0];
+      if(currentDateString === targetDateString){
+        this.mensagemHeader = 'Hoje a Rebecca voltou!';
+        return;
+      }
+      const timeDifference = targetDate.getTime() - currentDate.getTime();
+      this.daysRemaining = Math.ceil(timeDifference / (1000 * 3600 * 24));
+      if(this.daysRemaining == 1){
+        this.mensagemDia = 'dia';
+        this.mensagemFalta = 'Falta'; 
+      }
+      this.mensagemHeader = this.mensagemFalta + ' ' + this.daysRemaining + ' ' + this.mensagemDia + ' para a Rebecca voltar!';
+    }
+    else{
+      const timeDifference = targetDate.getTime() - currentDate.getTime();
+      this.daysRemaining = Math.ceil(timeDifference / (1000 * 3600 * 24));
+      if(this.daysRemaining == 1){
+        this.mensagemDia = 'dia';
+        this.mensagemFalta = 'Falta'; 
+      }
+      this.mensagemHeader = this.mensagemFalta + ' ' + this.daysRemaining + ' ' + this.mensagemDia + ' para a Rebecca se mudar :(';
+    }
+  }
+}
