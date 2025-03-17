29b5ce57478130a970d4843fe90f9208ff1fc335
Casasco <gabrielcasasco2@gmail.com>
2025-03-16 21:13:32 -0300

    Exibição de fotos funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { FotoComponent } from './componentes/foto/foto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContainerComponent, HeaderComponent],
+  imports: [RouterOutlet, ContainerComponent, HeaderComponent, FotoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'apprebecca';
+  currentDate!: Date;
+
+  ngOnInit(): void {
+    this.pegaDataAtual();
+  }
+
+  pegaDataAtual(): void {
+    this.currentDate = new Date();
+  }
 }
